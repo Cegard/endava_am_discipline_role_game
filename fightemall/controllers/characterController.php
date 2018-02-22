95cfbd6a79106325580ecec674a69075d9320ea4
Eduardo Galeano <egaleano@endava.com>
2018-02-22 18:23:21 -0500

    testHoomanCanAttack

---
+++ fightemall/controllers/characterController.php
@@ -0,0 +1,10 @@
+<?php namespace Game\Controllers;
+	
+	
+	class CharacterController{
+		
+		public static function attack($attacker, $defender){
+			
+		}
+	}
+?>
---
+++ fightemall/tests/utils.php
@@ -0,0 +1,7 @@
+<?php //namespace Tests;
+	
+	const OPTION_FOR_HOOMAN_SELECTION = 0;
+	const OPTION_FOR_DWARF_SELECTION = 1;
+	const OPTION_FOR_ELF_SELECTION = 2;
+	
+?>
